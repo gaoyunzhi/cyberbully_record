aecc56bb342983d845ae9bb89232ad19d5589dc9
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-04-16 07:41:59 -0700

    commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+import yaml
+
+output = ['# 网络暴力图鉴\n\n## 文章']
+
+with open('articles/meta.yaml') as f:
+	articles = yaml.load(f, Loader=yaml.FullLoader)
+for article in articles:
+	output.append('\n[%s](%s)' % (article['name'], article['link']))
+
+output.append('\n\n## 网络暴力公示')
+
+for dirname in filter(os.path.isdir, os.listdir('records')):
+	with open('records/%s/meta.yaml' % dirname) as f:
+		meta = yaml.load(f, Loader=yaml.FullLoader)
+	output.append('* 罪犯: ' + meta['name'])
+	# TODO: support other social accounts
+	output.append('* 微信号: ' + meta['wechat_id'])
+	output.append('* 标签: ' + ' '.join(meta['tags']))
+	output.append('* 详细信息: ' + ' '.join(meta['detail_link']))
+	output.append('* 图片: ' + ' '.join(meta['detail_link']))
+	output.append('''| <!-- -->    | <!-- -->    |
+|-------------|-------------|
+| Foo         | Bar         |''')
+
+
+
+with open('README.md', 'w') as f:
+	f.write(''.join(output))
+
+os.system('git add . > /dev/null 2>&1 && git commit -m commit > /dev/null 2>&1 && git push -u -f > /dev/null 2>&1')
+
+
+

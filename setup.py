5f1f4a8380ecf30f9faaf01d5eaca3f9ff83ec44
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-04-16 09:31:28 -0700

    commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from index import gen_index
 
 gen_readme()
 gen_index()
-os.system('git add . > /dev/null 2>&1 && git commit -m commit > /dev/null 2>&1 && git push -u -f > /dev/null 2>&1')
+# os.system('git add . > /dev/null 2>&1 && git commit -m commit > /dev/null 2>&1 && git push -u -f > /dev/null 2>&1')
 
 
 

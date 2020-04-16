59a67850b00ab59f8266a57f82f2a980fff9d7fa
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-04-16 08:45:50 -0700

    commit

--- image.py
+++ image.py
@@ -4,8 +4,7 @@ from bs4 import BeautifulSoup
 import math
 
 def get_single_image(image):
-	# max-width="500" max-height="800" 
-	return '<img src="' + image + '" style="max-width:100%;">'
+	return '<img width="500" height="800" src="' + image + '" style="max-width:100%;">'
 
 def get_image_line(images, width):
 	result = ['|' + get_single_image(x) for x in images]
